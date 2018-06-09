cc52bb8893615cffea2b4e43a85d81e8c88224f1
김혜인 <kendy93@sbcn.co.kr>
2018-06-09 16:42:32 +0900

    main 데이터 작업 완료

--- js/common.js
+++ js/common.js
@@ -2,27 +2,59 @@ class Movie {
     constructor () {
         this.state = {
             apiKey:'03244c916053ec8db051d6b477e36c1b',
-            language:'ko'
+            language:'ko',
         }
+    }
+    rankSlider() {
+        $('.rank_slider').slick({
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1,
+            arrows: false,
+        });
+
     }
     //main data
     popularData() {
+        const that = this;
         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${this.state.apiKey}&language=${this.state.language}`)
         .then(function (res) { return res.json() })
         .then(function (res) {
-            console.log(res)
+            const data = res.results;
+
+            //slider list
+            let html = '';
+            data.map(function(value,index){
+                html += `<div class="slider_list">
+                            <div class="tubmnaill" style="background-image: url(https://image.tmdb.org/t/p/w500/${value.poster_path})";>
+                                <span class="dim"></span>
+                            </div>
+                            <h2 class="title">${index+1}위 ${value.title}</h2>
+                            <p class="date">${value.release_date.replace(/-/gi,".")} 개봉</p>
+                            <div class="more_btn"><a href="./sub.html?${value.id}">MORE</a></div>
+                        </div>`
+            })
+
+            $('.rank_slider').html(html)
+
+            //slick slider 실행
+            that.rankSlider()
         })
-        .catch(function (err) { // 에러처리
+        .catch(function (err) { 
             console.error(err)
         })
     }
     //sub data
-    movieData(movieId) {
+    movieData() {
+        console.log(window.location.href)
+        const movieId = window.location.href.split('?')[1]
         fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${this.state.apiKey}&language=${this.state.language}`)
         .then(function (res) { return res.json() }) // return 값을 다음 then으로 넘겨줌
         .then(function (res) {
+            // window.href
             console.log(res)
-            return 123
+
+
         })
         .then(function(res){
             console.log(res)
@@ -33,18 +65,13 @@ class Movie {
 	}
 
 }
+
 const movie = new Movie();
 movie.popularData();
-movie.movieData(383498);
+movie.movieData();
+
 
 $(document).ready(function(){
-    $('.rank_slider').slick({
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1,
-        arrows: false,
-    });
-
     $('.photo_slider').slick({});
 
 });
