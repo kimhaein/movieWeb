06ee93ccf4eb4ac01d2da318c6619770e112da1e
김혜인 <kendy93@sbcn.co.kr>
2018-06-14 13:16:02 +0900

    [완료] 서브페이지 데이터 작업

--- js/sub.js
+++ js/sub.js
@@ -3,16 +3,21 @@ class MovieDetail {
         this.state = {
             apiKey:'03244c916053ec8db051d6b477e36c1b',
             language:'ko',
+            movieId: window.location.href.split('?')[1]
         }
+    }
+    init() {
+       this.getMovieData()  
+       this.getCastData()  
+       this.getImgData()  
+       this.getSimilarData()
     }
     photoSlider() {
         $('.photo_slider').slick({});
     }
     //sub data
     getMovieData() {
-        const that = this;
-        const movieId = window.location.href.split('?')[1]
-        fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${this.state.apiKey}&language=${this.state.language}`)
+        fetch(`https://api.themoviedb.org/3/movie/${this.state.movieId}?api_key=${this.state.apiKey}&language=${this.state.language}`)
         .then(function (res) { return res.json() }) 
         .then(function (res) {
 
@@ -29,11 +34,14 @@ class MovieDetail {
             $('.graph_wrap').find('.graph').css('width',(res.vote_average)*10+'%')
             $('.score').text((res.vote_average)*10)
 
-            this.photoSlider()
-
-            return fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${that.state.apiKey}`) 
-
         })
+        .catch(function (err) { // 에러처리
+            console.error(err)
+        })
+    }
+
+    getCastData() {
+        fetch(`https://api.themoviedb.org/3/movie/${this.state.movieId}/credits?api_key=${this.state.apiKey}`)   
         .then(function(res){ return res.json() })
         .then(function (res) {
             const cast = res.cast
@@ -50,15 +58,59 @@ class MovieDetail {
                 }
             })
             $('.cast_wrap').html(html)
-            console.log(cast)
         })
         .catch(function (err) { // 에러처리
             console.error(err)
         })
-	}
+    }
+
+    getImgData(){
+        const that = this
+        fetch(`https://api.themoviedb.org/3/movie/${this.state.movieId}/images?api_key=${this.state.apiKey}`)  
+        .then(function(res){ return res.json() })
+        .then(function (res) {
+            const img = res.backdrops
+            let html =''
+            img.map(function(value){
+                    html+= `<li style="background-image: url(https://image.tmdb.org/t/p/w500/${value.file_path})"></li>`
+            })
+
+            $('.photo_slider').html(html)
+            that.photoSlider()
+        })
+        .catch(function (err) { // 에러처리
+            console.error(err)
+        }) 
+    }
+
+    getSimilarData(){
+        fetch(`https://api.themoviedb.org/3/movie/${this.state.movieId}/similar?api_key=${this.state.apiKey}&language=${this.state.language}`)  
+        .then(function(res){ return res.json() })
+        .then(function (res) {
+            console.log(res)
+            const similar = res.results
+            let html =''
+            similar.map(function(value,index){
+                if(index<4){
+                    html+= `<div class="movie_list" style="background-image: url(https://image.tmdb.org/t/p/w500/${value.backdrop_path})">
+                                <a href="./sub.html?${value.id}">
+                                    <span class="dim">
+                                        <p>${value.title}</p>
+                                    </span>
+                                </a>
+                            </div>`
+                }
+            })
+
+            $('.recommend_movie').html(html)
+        })
+        .catch(function (err) { // 에러처리
+            console.error(err)
+        }) 
+    }
 
 }
 
 const movieDetail = new MovieDetail();
-movieDetail.getMovieData().bind(this);
+movieDetail.init();
 
